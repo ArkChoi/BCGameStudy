058c7d070234875c0491a61b690d64074fcd95d0
ArkChoi <ark.goya@gmail.com>
2025-10-21 17:13:59 +0900

    SetUp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
 #include "Engine.h"
 #include "DynamicArray.h"
 
 int main()
 {
-	/*GEngine->Init();
+	GEngine->Init();
 	GEngine->Run();
 	GEngine->Term();
 
-	delete GEngine;*/
-
-	std::vector <int>test;
-	test.push_back(1);
-
-	TDynamicArray <int>Test;
-	Test.PushBack(1);
-	Test.PushBack(3);
-	Test.PushBack(5);
-	Test.PushBack(7);
-
-	/*Test.AllValuePrint();
-
-	std::cout << std::endl;
-	std::cout << Test.GetSize() << std::endl;
-	std::cout << std::endl;
-
-	for (int i = 0; i < 10; i++)
-	{
-		Test.PushBack(i);
-	}
-
-	Test.AllValuePrint();
-
-	std::cout << std::endl;
-	std::cout << Test.GetSize() << std::endl;
-	std::cout <<  std::endl;*/
-
-	std::cout << std::endl;
-	for (size_t i = 0; i < Test.GetSize(); i++)
-	{
-		std::cout << Test[i] << std::endl;
-	}
-	std::cout << std::endl;
+	delete GEngine;
 
 	return 0;
 }
