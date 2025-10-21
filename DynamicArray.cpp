65f8f7de3902b1481cd20e3bab4618b5797a066d
ArkChoi <ark.goya@gmail.com>
2025-10-21 15:28:38 +0900

    DynamicArray^2

--- DynamicArray.cpp
+++ DynamicArray.cpp
@@ -13,16 +13,20 @@ TDynamicArray::~TDynamicArray()
 
 void TDynamicArray::PushBack(int Value)
 {
-	if (Index < Size)
+	if (Index < Capacity)
 	{
 		Data[Index] = Value;
 		Index++;
+		Size++;
 	}
 	else
 	{
-		Size += 1;
-		int* NewArray = new int[Size];
-		for (int i = 0; i < (Size-1); i++)
+		if (Capacity == 0)
+		{
+			Capacity = 1;
+		}
+		int* NewArray = new int[Capacity*2];
+		for (int i = 0; i < Size; i++)
 		{
 			NewArray[i] = Data[i];
 		}
@@ -32,6 +36,9 @@ void TDynamicArray::PushBack(int Value)
 
 		Data[Index] = Value;
 		Index++;
+		Size++;
+
+		Capacity *= 2;
 	}
 }
 
--- DynamicArray.h
+++ DynamicArray.h
@@ -7,7 +7,10 @@ public:
 	virtual ~TDynamicArray();
 
 	int* Data;
-	int Size = 0;
+
+	int Size = 0; //실 자료 입력 양
+	int Capacity = 0; //자료가 잡혀있는 크기
+
 	int Index = 0;
 
 	void PushBack(int Value);
